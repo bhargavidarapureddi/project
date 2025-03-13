2e06b4bb76c293a1ee66312010bfff9f079a6f21
bhargavidarapureddi <116108219+bhargavidarapureddi@users.noreply.github.com>
2025-03-13 12:50:58 +0530

    Delete A-sko directory

--- A-sko/ProductService/SnowflakeCreds.py
+++
@@ -1,8 +0,0 @@
-
-snowflake_config = { 
-                    'account' : 'vccevuc-sa96036',
-                    'user': 'keerthanjj',
-                    'password' : 'Mypwsnow123@',
-                    'database' : 'ESKO',
-                    'schema' : 'ESKO/PUBLIC'
-}
